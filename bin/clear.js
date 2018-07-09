4306ac2da5474f9abc77a10d2b61aaeef322f560
WeiJietao <13415156317@163.com>
2018-07-09 21:28:34 +0800

    多页面配置打包测试

--- bin/clear.js
+++
@@ -1,6 +0,0 @@
-let fs = require("fs");
-let path = require("path");
-
-let content = fs.readFileSync(path.resolve(__dirname, "./../index.html"), "utf-8");
-content = content.replace(/\.\/\.\.\/dist\//g, "https://weijietao.github.io/wjt20/dist/");
-fs.writeFileSync(path.resolve(__dirname, "./../index.html"), content, "utf-8");
