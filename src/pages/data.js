6f65a0fdffe0a318bb403b722daf00e76d4fb330
WeiJietao <13415156317@163.com>
2019-03-04 22:03:33 +0800

    feat: 更新数据文件

--- src/pages/data.js
+++ src/pages/data.js
@@ -1,6 +1,4 @@
 // 静态数据
 export default {
-    ...require('./../../database/md-data1.json'),
-    ...require('./../../database/md-data2.json'),
-    ...require('./../../database/md-data3.json')
+    ...require('./../../database/all1.json')
 };
